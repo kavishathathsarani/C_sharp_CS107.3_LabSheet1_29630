24449232665a8d56c868abb5c43ed8e68c0a42dc
kavishathathsarani_USER_ <kaviherath377@gmail.com_USER_@DESKTOP-2SR3E2E>
2024-02-08 06:25:50 -0800

    Add project files.

---
+++ labsheet1Q1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the length ");
+            int h = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please enter the width ");
+            int w = Convert.ToInt32(Console.ReadLine());
+            CalArea(w, h);
+            Console.ReadLine();
+        }
+        static void CalArea(int length, int height)
+        {
+            int area;
+            area = height * length;
+            Console.WriteLine("The area is " + area);
+        }
+    }
+}
