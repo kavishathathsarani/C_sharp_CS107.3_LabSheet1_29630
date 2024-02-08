8c058c1dee9d74f3fa11c077026d4c299efc1d32
kavishathathsarani_USER_ <kaviherath377@gmail.com_USER_@DESKTOP-2SR3E2E>
2024-02-08 07:12:00 -0800

    upload labsheet

---
+++ labsheet1Q2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    /* Write a Console Application program to check if the given 10 number inputs are even or odd
+     Prompt the user to enter the numbers 
+     and display whether it's even or odd
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("Enter a number ");
+                int number = Convert.ToInt32(Console.ReadLine());
+
+                if (number % 2 == 0)
+                {
+                    Console.WriteLine("It is a Even number");
+                    Console.ReadLine();
+                }
+
+                else
+                {
+                    Console.WriteLine("It is a Odd number");
+                    Console.ReadLine();
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
